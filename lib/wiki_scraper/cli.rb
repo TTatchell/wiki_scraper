51f1021d5b8935287eaaf34282431d00b68b5ab8
TTatchell <tim.tatchell24@gmail.com>
2021-08-11 21:40:11 +1000

    Adding some foundation to cli.rb

--- lib/wiki_scraper/cli.rb
+++ lib/wiki_scraper/cli.rb
@@ -1,1 +1,28 @@
-#This is where app will start and run
+class WikiScraper
+  def initialize
+    cli
+  end
+
+  def blank
+    puts "\e[2J\e[f"
+  end
+
+  def line
+    puts "##############################################"
+  end
+
+  def cli
+    blank
+    line
+    puts "           Welcome To WikiScraper!            "
+    line
+    sleep(3)
+    blank
+    line
+    puts "Let's check out an article:"
+    line
+    sleep(3)
+  end
+end
+
+WikiScraper.new
